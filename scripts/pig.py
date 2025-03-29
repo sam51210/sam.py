effd9a5c9a55cc1dfe2b66b008fa98944d960877
Sam <63399466+sam51210@users.noreply.github.com>
2025-03-29 14:00:07 +0000

    Update pig.py

--- scripts/pig.py
+++ scripts/pig.py
@@ -1,30 +1,22 @@
-if scripto.upper() == 'PIG.PY': # Sampyhub Exclusive
-    search("pig.py","Daniel Schroeder") # Sampyhub Exclusive
-    clear() # Sampyhub Exclusive
+elif scripto.upper() == 'PIG.PY':
+    search("pig.py","Daniel Schroeder")
+    clear()
     
     from random import randint
 
+    def check_die(score, die_value):
+        return 0 if die_value == 1 else score + die_value
 
-def check_die(score, die_value):
-    if die_value == 1:
-        return 0
-    else:
-        return score + die_value
+    def display_scoreboard(player_score, computer_score):
+        print("\n" + "#" * 20)
+        print(f"Player Score: {player_score}")
+        print(f"Computer Score: {computer_score}")
+        print("#" * 20 + "\n")
 
+    player_score = 0
+    computer_score = 0
 
-def display_scoreboard(player_score, computer_score):
-    print()
-    print("#" * 20)
-    print(f"Player Score: {player_score}")
-    print(f"Computer Score: {computer_score}")
-    print("#" * 20)
-    print()
-
-
-player_score = 0
-computer_score = 0
-
-welcome_message = """
+    print("""
           Welcome to 'Pig', a dice game!
     
     In this game, a user and a computer opponent 
@@ -33,31 +25,29 @@ welcome_message = """
     all of their points. Otherwise, the player gets the
     value of the die added to their points. The first
     player to reach 30 points wins!
-"""
+    """)
 
-print(welcome_message)
+    username = input("What is your name? ")
 
-username = input("What is your name? ")
+    while True:
+        input(f"Press 'Enter' to roll the die, {username}!\n")
+        player_die_value = randint(1, 6)
+        print(f"{username} rolls a {player_die_value}")
 
-while True:
-    input(f"Press 'Enter' to roll the die {username}!\n")
+        computer_die_value = randint(1, 6)
+        print(f"Computer rolls a {computer_die_value}")
 
-    player_die_value = randint(1, 6)
-    print(f"{username} rolls a {player_die_value}")
+        player_score = check_die(player_score, player_die_value)
+        computer_score = check_die(computer_score, computer_die_value)
 
-    computer_die_value = randint(1, 6)
-    print(f"Computer rolls a {computer_die_value}")
+        display_scoreboard(player_score, computer_score)
 
-    player_score = check_die(player_score, player_die_value)
-    computer_score = check_die(computer_score, computer_die_value)
+        if player_score >= 30:
+            print(f"{username} wins!")
+            break
+        elif computer_score >= 30:
+            print("Computer wins!")
+            break
 
-    display_scoreboard(player_score, computer_score)
-
-    if player_score >= 30:
-        print(f"{username} wins!")
-        break
-    elif computer_score >= 30:
-        print("Computer wins!")
-        break
-    
-end() # Sampyhub Exclusive
+    # End the Pig game properly
+    end()
