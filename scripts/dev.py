335727852b13faa8047520c485eff7054ad62b40
Sam <63399466+sam51210@users.noreply.github.com>
2025-03-29 14:10:51 +0000

    Create dev.py

---
+++ scripts/dev.py
@@ -0,0 +1,20 @@
+elif scripto.upper() == 'DEV.PY':
+    search("dev.msg","sam51210") # Sam.py MESSAGE
+    clear()
+    
+    maininfo = """
+            Welcome to the Sam.py development community!
+            
+            We are so happy you're interested in helping out. It's
+            not difficult to modify the code as it's well documented
+            and commented. It's not too advanced - but we welcome
+            any skills levels!
+            
+            With that said, get coding and send it over to us on Discord
+            by DMing @sam51211. You will always be mentioned in the
+            credits.py module, even if you edit one line of code!
+            """
+    print(maininfo)
+    print("")
+    input("Press Enter to continue...")
+    end()
